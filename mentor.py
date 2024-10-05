2470a596779aac609d20df31758483ff8b418349
Siara Small <41011312+siarasmall@users.noreply.github.com>
2024-10-05 14:58:50 -0400

    Create mentor.py

---
+++ mentor.py
@@ -0,0 +1,44 @@
+from typing import List
+from mentee import Mentee
+
+class Mentor:
+    def __init__(self, name: str, email: str, grade: str, classInterest: int, researchInterest: int, industryInterest: int, gradInterest: int, pivotInterest: int, mentees: List[Mentee]):
+        self.name = name
+        self.email = email
+        self.grade = grade
+        self.classInterest = classInterest
+        self.researchInterest = researchInterest
+        self.industryInterest = industryInterest
+        self.gradInterest = gradInterest
+        self.pivotInterest = pivotInterest
+        self.mentees = mentees
+
+    def addMentee(self, mentee: Mentee):
+        self.mentees.append(mentee)
+
+    def getMentees(self):
+        return self.mentees
+
+    def getName(self):
+        return self.name
+    
+    def getEmail(self):
+        return self.email
+
+    def getGrade(self):
+        return self.grade
+    
+    def getClassInterest(self):
+        return self.classInterest
+
+    def getResearchInterest(self):
+        return self.researchInterest
+
+    def getIndustryInterest(self):
+        return self.industryInterest
+
+    def getGradInterest(self):
+        return self.gradInterest
+
+    def getPivotInterest(self):
+        return self.pivotInterest
